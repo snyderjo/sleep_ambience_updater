a63ee717b9d463da852937efb40255c44359ffcd
John Snyder <snyderjo@gmail.com>
2025-03-23 21:40:54 -0600

    amends the updater-dag to make use of a different directory, and which files are copied and pushed/tracked.

--- dags/sleep_ambience_updater.py
+++ dags/sleep_ambience_updater.py
@@ -16,22 +16,22 @@ with DAG(
     ,description = 'this renders predefined rmarkdown reports weekly, and pushes them to a github.io project'
     ,start_date=datetime(2025,3,10,tzinfo=pendulum.timezone("America/Denver"))
     ,catchup = True
-    ,schedule_interval = '00 9 * * *'
+    ,schedule_interval = '00 9 * * 1'
 ) as dag:
     
     report_render = BashOperator(
         task_id = 'index_rmd'
-        ,bash_command = "cd {{ var.value.r_dir_ambComp }}; Rscript renderer.R"
+        ,bash_command = "cd {{ var.value.r_dir_sleepAmb }}; Rscript renderer.R"
     )
 
     mv_results = BashOperator(
         task_id='mv_to_github'
-        ,bash_command = "cd {{ var.value.r_dir_ambComp }}; mv -f index.md dailyReport.html {{ var.value.githubIO_dir }}ambience_sleep/; mv -f images/*.png {{ var.value.githubIO_dir }}ambience_sleep/images/"
+        ,bash_command = "cd {{ var.value.r_dir_sleepAmb }}; mv -f index.md weeklyReport.html {{ var.value.githubIO_dir }}ambience_sleep/; mv -f images/*.png {{ var.value.githubIO_dir }}ambience_sleep/images/"
     )
 
     git_push = BashOperator(
         task_id = "update_githubIO"
-        ,bash_command = "cd {{ var.value.githubIO_dir }}ambience_sleep/; git add index.md dailyReport.html images/*.png; git commit -m 'updates results on {{ ds }}'; git push origin master"
+        ,bash_command = "cd {{ var.value.githubIO_dir }}ambience_sleep/; git add index.md weeklyReport.html images/*.png; git commit -m 'updates results on {{ ds }}'; git push origin master"
     )
 
     report_render >> mv_results >> git_push 
